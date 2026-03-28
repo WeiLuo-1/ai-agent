e60670dfcb1c8f366a13affe2bdeb4a3618f92f6
WeiLuo-1 <luo.1310@osu.edu>
2026-03-28 17:43:37 -0500

    Different ways of AI invoke

---
+++ src/main/java/com/wei/aiagent/demo/invoke/LangChainAiInvoke.java
@@ -0,0 +1,15 @@
+package com.wei.aiagent.demo.invoke;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+
+public class LangChainAiInvoke {
+    public static void main(String[] args) {
+        ChatLanguageModel qwenChatModel = QwenChatModel.builder()
+                .apiKey(TestApiKey.API_KEY)
+                .modelName("qwen-max")
+                .build();
+        String answer = qwenChatModel.chat("I'm a software engineer");
+        System.out.println(answer);
+    }
+}
---
+++ src/main/java/com/wei/aiagent/demo/invoke/TestApiKey.java
@@ -0,0 +1,9 @@
+package com.wei.aiagent.demo.invoke;
+
+/**
+ * only for test
+ */
+public interface TestApiKey {
+
+    String API_KEY = "apiKey";
+}
